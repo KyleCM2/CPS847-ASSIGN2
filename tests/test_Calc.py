a95c6a568bdec12ab9c3bef13ebb32927e9219c2
Kyle C <54860835+KyleCM2@users.noreply.github.com>
2021-04-01 23:47:10 -0400

    Create test_Calc.py

---
+++ tests/test_Calc.py
@@ -0,0 +1,11 @@
+from Calculator import Calculator
+
+def test_add():
+    x,y = 1,2
+    instance = DummyCode(x,y)
+    assert instance.add() == x+y, "add method doesnt work"
+
+def test_subtract():
+    x,y = 1,2
+    instance = DummyCode(x,y)
+    assert instance.subtract() == x-y, "subtract method doesnt work"
