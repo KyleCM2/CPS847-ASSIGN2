8c96373ce41ad55517eb50bee7e9ee50ffef259a
Kyle C <54860835+KyleCM2@users.noreply.github.com>
2021-04-01 23:51:08 -0400

    Update test_Calc.py

--- tests/test_Calc.py
+++ tests/test_Calc.py
@@ -1,9 +1,9 @@
 def test_add():
     x,y = 1,2
-    instance = DummyCode(x,y)
+    instance = Calculator(x,y)
     assert instance.add() == x+y, "add method doesnt work"
 
 def test_subtract():
     x,y = 1,2
-    instance = DummyCode(x,y)
+    instance = Calculator(x,y)
     assert instance.subtract() == x-y, "subtract method doesnt work"
